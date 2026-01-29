5e53838d55134dc4e4eb342e4c9595bd3eef5efb
ashsantos-ky <santos.ay@obmontessori.edu.ph>
2026-01-29 11:08:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyscript import display, document
 def intrams_checker(e):
     registration = document.querySelector("input[name='registration']:checked").value
     clearance = document.querySelector("input[name='clinic']:checked").value
-
+# to apply 
     if registration == 'yes' and clearance == 'yes':
         display("You are allowed to take the intrams.", target='output')
     else:
